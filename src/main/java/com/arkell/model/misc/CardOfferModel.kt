533a299b132d30cb2c1049b42c781f9738da922a
NikichXP <nikichx2@gmail.com>
2019-03-20 22:15:12 +0200

    id-generator for card offer

--- src/main/java/com/arkell/model/misc/CardOfferModel.kt
+++ src/main/java/com/arkell/model/misc/CardOfferModel.kt
@@ -11,7 +11,16 @@ import org.springframework.stereotype.Service
 class CardOfferModel(
 		override val repository: CardOfferRepo) : UpdateAction<CardOffer>() {
 
-	fun addCardOffer(data: Map<String, String>): CardOffer = save(ObjectFromMapUpdater(CardOffer()).data(data).modify())
+	fun addCardOffer(data: Map<String, String>): CardOffer = save(
+			ObjectFromMapUpdater(CardOffer()).data(data).modify().apply {
+				var i = 0
+
+				while (repository.findById("card-offer-$i").isPresent) {
+					i++
+				}
+
+				id = "card-offer-$i"
+			})
 
 	fun edit(id: String, data: Map<String, String>) = autoEdit(id, data, *Excludes.default) {}
 	fun list(): List<CardOffer> {
